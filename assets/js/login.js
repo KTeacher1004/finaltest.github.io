3c8775c635afb3c02ae6fc170db9d1363cd446a1
jaki5646 <vinhlocson@gmail.com>
2023-12-06 20:03:55 +0700

    update the verifier pending: login check for user.json

--- assets/js/login.js
+++ assets/js/login.js
@@ -9,12 +9,5 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
 })
 submitBtn.addEventListener('click', (e) => {
-    if(password.value.length < 8 ||password) {
-        passwordText.innerHTML = 'Incorrect password!'
-    }
-    else{
-        passwordText.innerHTML = ''
-    }
+    console.log('later lmfao')
 })
-let a = 'sach7774'
-console.log(a.substring(4))
