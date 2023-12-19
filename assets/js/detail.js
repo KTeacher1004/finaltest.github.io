f9cb14a6be05918d126e7a97bd95c10aca19c804
Gokuktkn <120592109+Gokuktkn@users.noreply.github.com>
2023-12-19 11:26:35 +0700

    Update trang detail và homepage(landing page)
    chỉnh sửa design home, responsive cho 2  trang

--- assets/js/detail.js
+++ assets/js/detail.js
@@ -1,13 +1,65 @@
+// set listBook detail
+let listBook = [
+    {
+        key: "#heart",
+        name: "Heart berries",
+        desc: "The debut book from the  Terese Marie, a Canadian writer. Writer is going to witness Mailhot and her complicated childhood and tumultuous motherhood. We are going to witness all of Maillot mental health and identity",
+        price: 120000,
+        listBookImg: ["./assets/images/History book/heart berries/heart berries.png", "./assets/images/History book/heart berries/side image heart 1.png", "./assets/images/History book/heart berries/side image heart 2.png", "./assets/images/History book/heart berries/side image heart 3.png"]
+    },
+    {
+        key: "#steel",
+        name: "Steel",
+        desc: "The book written by Nikolai A. Ostrovsky. The story about main young man name Pavel Korchagin. Pavel symbolises mans in the revolution in Russian. His transformation from young boy to a teenager and then becomes a tough grown man. His journey suffer lot of pain and he has to do dangerous missions to become a man do everything that country offer him",
+        price: 150000,
+        listBookImg: ["./assets/images/History book/steel/Steel.png", "./assets/images/History book/steel/steel side image 1.png", "./assets/images/History book/steel/steel side image 2.png", "./assets/images/History book/steel/steel side image 3.png"]
+    },
+    {
+        key: "#liars",
+        name: "Liarclub",
+        desc: "The book written by Mary Karr. The story tell her childhood in 1960s in a small industrial town in Texas.Her town is the place where alcohol abuse and phychological problems are common issues",
+        price: 120000,
+        listBookImg: ["./assets/images/History book/the liars club/the liars club.png", "./assets/images/History book//the liars club/the liar club side image 1.png", "./assets/images/History book//the liars club/the liar club side image 2.png", "./assets/images/History book//the liars club/the liar club side image 3.png"]
+    },
+    {
+        key: "#Sher1",
+        name: "Sherlock holmes 1",
+        desc: "This is the first edition of Sherlock Holmes series which are written by Sir Conan Doyle. Thrilled, Detective, Interesting novel with lot of plot-twist and creative, extraordinary ways of solving crime",
+        price: 150000,
+        listBookImg: ["./assets/images/Detective/Sher1/main.png", "./assets/images/Detective/Sher1/side image 1.png", "./assets/images/Detective/Sher1/side image 2.png", "./assets/images/Detective/Sher1/side image 3.png"]
+    },
+    {
+        key: "#Sher2",
+        name: "Sherlock holmes 2",
+        desc: "This is the second edition of Sherlock Holmes series after the previous Sherlock Holmes 1. We are going to immerse into a Sherlock Holmes world with a lot of bizzare adventure with Sherlock Holmes and his fellow friend, Dr Watson. This adventure is going to break your mind a lot.",
+        price: 150000,
+        listBookImg: ["./assets/images/Detective/Sher2/main.png", "./assets/images/Detective/Sher2/side image 1.png", "./assets/images/Detective/Sher2/side image 2.png", "./assets/images/Detective/Sher2/side image 3.png"]
+    },
+    {
+        key: "#Sher3",
+        name: "Sherlock holmes 3",
+        desc: "This is the third and last edition of Sherlock Holmes series after the previous Sherlock Holmes 1. More bizzare, More excitement, More crime, More action. That is the slogan of this edition",
+        price: 150000,
+        listBookImg: ["./assets/images/Detective/Sher3/main.png", "./assets/images/Detective/Sher3/side image 1.png", "./assets/images/Detective/Sher3/side image 2.png", "./assets/images/Detective/Sher3/side image 3.png"]
+    }
+]
+// image variable
 let mainImg = document.querySelector("#main-img");
 let listImg = document.querySelectorAll(".list-img img");
 let prevBtn = document.getElementsByClassName("prev")[0];
 let nextBtn = document.getElementsByClassName("next")[0];
 let currentImgIndex = 0;
+// quantity variable
+let number = document.getElementById("number");
+let numValue = number.value;
+
+let locationBook = window.location.hash;
+let bookDetail = listBook.find(item => item.key === locationBook);
 
 // change current img
 function updateMainImg(index) {
     // remove active
-    document.querySelectorAll(".list-img div").forEach(item =>{
+    document.querySelectorAll(".list-img div").forEach(item => {
         item.classList.remove("active");
     })
     currentImgIndex = index;
@@ -20,29 +72,75 @@ listImg.forEach((imgElement, index) => {
         updateMainImg(index)
     })
 })
-prevBtn.addEventListener("click", e=>{
+prevBtn.addEventListener("click", e => {
     if (currentImgIndex === 0) {
         currentImgIndex = listImg.length - 1;
-    }else{
+    } else {
         currentImgIndex--;
     }
     updateMainImg(currentImgIndex);
 })
-nextBtn.addEventListener("click", e=>{
+nextBtn.addEventListener("click", e => {
     if (currentImgIndex === listImg.length - 1) {
         currentImgIndex = 0;
-    }else{
+    } else {
         currentImgIndex++;
     }
     updateMainImg(currentImgIndex);
 })
-updateMainImg(0)
 
-let add = document.getElementById("add-btn");
-let minus = document.getElementById("minus-btn");
-let number = document.getElementById("number").value;
+// quantity control
+let render = (numValue) => {
+    number.value = numValue;
+}
 
-add.addEventListener("click", e=>{
-    number++;
-    
+let add = () => {
+    numValue++;
+    render(numValue);
+}
+let minus = () => {
+    if (numValue > 1) {
+        numValue--;
+        render(numValue);
+    }
+}
+number.addEventListener("input", () => {
+    numValue = number.value;
+    numValue = parseInt(numValue);
+    numValue = (isNaN(numValue) ? 1 || numValue === 0 : numValue);
+    render(numValue)
 })
+
+// buy button event
+let buy = () => {
+    alert("Bạn đã mua thành công! Số tiền cần trả là: " + addDot((numValue * bookDetail.price).toString()) + "đ");
+}
+
+
+function loadInfo() {
+    let bookName = document.getElementById("name");
+    let desc = document.getElementById("desc");
+    let price = document.getElementById("price");
+    bookName.innerText = bookDetail.name;
+    desc.innerText = bookDetail.desc;
+    price.innerText = addDot(bookDetail.price.toString());
+    for (let i = 0; i < listImg.length; i++) {
+        listImg[i].src = bookDetail.listBookImg[i];
+    }
+    updateMainImg(0)
+}
+function addDot(a) {
+    let priceTest = "";
+    let count = 0;
+    for (let i = a.length - 1; i >= 0; i--) {
+        priceTest+=a[i];
+        count++
+        if (count==a.length) {
+            break;
+        }
+        else if (count % 3 == 0) {
+            priceTest+= ".";
+        }
+    }
+    return a = priceTest.split("").reverse().join("");
+}
--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,31 +1,31 @@
-const bigcontainer = document.querySelector('.container');
-const bigcontainer2 = document.querySelector('.container2');
-const bigcontainer3 = document.querySelector('.container3');
+const detective = document.querySelector('.detective-book');
+const history = document.querySelector('.history-book');
+const producer = document.querySelector('.producer');
 
 
 var list1 = [
     {
         name: 'Sherlock holmes 1',
         image: './assets/images/Detective/Sher1/main.png',
-        price: 5.99,
+        price: "150000",
         link: 'detail.html#Sher1',
         introduction: 'In the shadowy streets of Victorian London, a brilliant detective named Sherlock Holmes and his loyal companion, Dr. John Watson, embark on their first adventure together, plunging headlong into a thrilling mystery that will test their intelligence and courage. '
     },
 
     {
-    name: 'Sherlock holmes 2',
-    image: './assets/images/Detective/Sher2/main.png',
-    price: 5.99,
-    link: 'detail.html#Sher2',
-    introduction: 'The game is afoot once more as Sherlock Holmes and Dr. Watson return in a thrilling sequel! '
+        name: 'Sherlock holmes 2',
+        image: './assets/images/Detective/Sher2/main.png',
+        price: "150000",
+        link: 'detail.html#Sher2',
+        introduction: 'The game is afoot once more as Sherlock Holmes and Dr. Watson return in a thrilling sequel! '
     },
 
     {
-    name: 'Sherlock holmes 3',
-    image: './assets/images/Detective/Sher3/main.png',
-    price: 5.99,
-    link: 'detail.html#Sher3',
-    introduction: `Shadows deepen, secrets unravel, and a new puzzle awaits the world's greatest detective, Sherlock Holmes, in the highly anticipated third installment of his thrilling adventures. Prepare to be captivated once more as the iconic duo of Holmes and Watson delve into a web of intrigue that will challenge their minds and test their unwavering loyalty to justice.`
+        name: 'Sherlock holmes 3',
+        image: './assets/images/Detective/Sher3/main.png',
+        price: "150000",
+        link: 'detail.html#Sher3',
+        introduction: `Shadows deepen, secrets unravel, and a new puzzle awaits the world's greatest detective, Sherlock Holmes, in the highly anticipated third installment of his thrilling adventures. Prepare to be captivated once more as the iconic duo of Holmes and Watson delve into a web of intrigue that will challenge their minds and test their unwavering loyalty to justice.`
     }
 ]
 
@@ -33,7 +33,7 @@ var list2 = [
     {
         name: 'Heart Berries',
         image: './assets/images/History book/heart berries/heart berries.png',
-        price: 4.99,
+        price: "120000",
         link: 'detail.html#heart',
         introduction: `Prepare to embark on a poignant and powerful journey through love, loss, and resilience as you delve into the award-winning memoir "Heart Berries," where Terese Marie Mailhot paints a captivating portrait of her life as a First Nations woman navigating the complexities of family, self-discovery, and the enduring spirit of her people.`
     },
@@ -41,7 +41,7 @@ var list2 = [
     {
         name: 'Steel',
         image: './assets/images/History book/steel/Steel.png',
-        price: 5.99,
+        price: "150000",
         link: 'detail.html#steel',
         introduction: `
         In the turbulent times of the Russian Civil War and the rise of the Soviet Union, witness the captivating story of Pavel Korchagin, a young man whose trials and tribulations forge him into a true hero.`
@@ -49,8 +49,8 @@ var list2 = [
 
     {
         name: 'Liar club',
-        image: './assets/images/History book/steel/the liars club.png',
-        price: 4.99,
+        image: './assets/images/History book/the liars club/the liars club.png',
+        price: "120000",
         link: 'detail.html#liars',
         introduction: `n Mary Karr's captivating memoir, "The Liars' Club," we embark on a poignant journey through the author's childhood in a small, oil-boom town in Southeast Texas. Through vivid storytelling and insightful reflection, Karr explores themes of family dynamics, societal expectations, and the complex relationship between truth and fiction in the formative years.`
     },
@@ -66,7 +66,7 @@ var list3 = [
         name: 'harvard',
         link: './assets/images/Producer/harvard.jpg'
     },
-    
+
     {
         name: 'nha nam',
         link: './assets/images/Producer/nha-nam.jpg'
@@ -86,54 +86,47 @@ var list3 = [
         name: 'penguin',
         link: './assets/images/Producer/penguin.jpg'
     },
-
-
 ]
 
-for (let i = 0; i < list1.length; i++) {
-    let box = document.createElement('a');
-    box.classList.add('bookcontainer'); 
-    box.href = list1[i].link;
-    let heading = document.createElement('h4');
-    heading.innerText = list1[i].name;
-    let making = document.createElement('img');
-    making.src = list1[i].image; 
-    let cost = document.createElement('strong')
-    cost.innerText = `${list1[i].price}$`
-
+function loadBook(list, bookType) {
+    for (let i = 0; i < list.length; i++) {
+        let box = document.createElement('a');
+        box.classList.add('bookcontainer');
+        box.href = list[i].link;
+        let heading = document.createElement('h4');
+        heading.innerText = list[i].name;
+        let making = document.createElement('img');
+        making.src = list[i].image;
+        let cost = document.createElement('strong')
+        cost.innerText = addDot((`${list[i].price}`)) + "đ";
     
-    box.appendChild(making);
-    box.appendChild(heading);
-    box.appendChild(cost);
-    bigcontainer.appendChild(box);
-    
-  
+        box.appendChild(making);
+        box.appendChild(heading);
+        box.appendChild(cost);
+        bookType.appendChild(box);
+    }
 }
 
-
-for (let i = 0; i < list2.length; i++) {
-    let box = document.createElement('a');
-    box.classList.add('bookcontainer'); 
-    box.href = list2[i].link;
-    let heading = document.createElement('h4');
-    heading.innerText = list2[i].name;
-    let making = document.createElement('img');
-    making.src = list2[i].image; 
-    let cost = document.createElement('strong')
-    cost.innerText = `${list2[i].price}$`
-
-    
-    box.appendChild(making);
-    box.appendChild(heading);
-    box.appendChild(cost);
-    bigcontainer2.appendChild(box);
-    
-  
-}
-
+loadBook(list1, detective);
+loadBook(list2, history);
 
 for (let i = 0; i < list3.length; i++) {
     let pic = document.createElement('img');
     pic.src = list3[i].link;
-    bigcontainer3.appendChild(pic);
+    producer.appendChild(pic);
+}
+function addDot(a) {
+    let priceTest = "";
+    let count = 0;
+    for (let i = a.length - 1; i >= 0; i--) {
+        priceTest+=a[i];
+        count++
+        if (count==a.length) {
+            break;
+        }
+        else if (count % 3 == 0) {
+            priceTest+= ".";
+        }
+    }
+    return a = priceTest.split("").reverse().join("");
 }
